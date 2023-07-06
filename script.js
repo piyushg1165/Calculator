1aaf63cac31168b34df12b21e91acf8c3368acd5
Piyush Gupta <anshg1165@gmail.com>
2023-07-06 13:44:40 +0530

    stable

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
@@ -27,33 +26,3 @@ buttons.forEach((button) => {
 
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
-=======
-const display = document.querySelector(".display");
-const buttons = document.querySelectorAll("button");
-const specialChars = ["%", "*", "/", "-", "+", "="];
-let output = "";
-
-const calculate = (btnValue) => {
-  display.focus();
-  if (btnValue === "=" && output !== "") {
-
-    output = eval(output.replace("%", "/100"));
-  } else if (btnValue === "AC") {
-    output = "";
-  } else if (btnValue === "DEL") {
-
-    output = output.toString().slice(0, -1);
-  } else {
-
-    if (output === "" && specialChars.includes(btnValue)) return;
-    output += btnValue;
-  }
-  display.value = output;
-};
-
-
-buttons.forEach((button) => {
-
-  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
-});
->>>>>>> 73cfcf27580de1fbc96526f01e2cd10aed6046f6
